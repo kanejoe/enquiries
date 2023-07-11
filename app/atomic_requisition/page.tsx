654ad639dc135f0a0063257f96851da0b7de2fe9
kanejoe <kane_joseph@hotmail.com>
2023-07-11 21:57:55 +0100

    adding some media query touches

--- app/atomic_requisition/page.tsx
+++ app/atomic_requisition/page.tsx
@@ -11,15 +11,22 @@ const req = {
     "if so, state whether by Irish Water mains, on-site domestic septic tank or other on-site domestic waste water treatment system, or other",
   reply: "not applicable",
   level: 2,
-  replyReqired: true,
+  replyRequired: true,
 }
 const req1 = {
   reqId: "R43FKLF",
   clauseRef: "2.1",
-  query: "mains or something else",
+  query: "Is the property serviced with:",
   reply: "not applicable",
   level: 1,
-  replyReqired: true,
+  replyRequired: false,
+}
+const req0 = {
+  reqId: "43FJK",
+  clauseRef: "2.",
+  query: "Water Services/Local Authority",
+  level: 0,
+  reqIsApplicable: false,
 }
 
 interface AtomicRequisitionProps {
@@ -35,14 +42,23 @@ export default function AtomicRequisitionPage() {
   return (
     <section className="mt-16">
       <h1 className="text-xl font-semibold">Atomic Requisition</h1>
-      <div className="mt-6 rounded p-6 flex flex-col">
+      <div className="mt-6 flex flex-col rounded p-6">
+        {req0.level === 0 ? (
+          <AtomicRequisitionHeading
+            reqId={req0.reqId}
+            clauseRef={req0.clauseRef}
+            query={req0.query}
+            level={req0.level}
+            reqIsApplicable={req0.reqIsApplicable}
+          />
+        ) : null}
         <AtomicRequisition
           reqId={req1.reqId}
           clauseRef={req1.clauseRef}
           query={req1.query}
           reply={req1.reply}
           level={req1.level}
-          replyRequired={req1.replyReqired}
+          replyRequired={req1.replyRequired}
         />
         <AtomicRequisition
           reqId={req.reqId}
@@ -50,10 +66,33 @@ export default function AtomicRequisitionPage() {
           query={req.query}
           reply={req.reply}
           level={req.level}
-          replyRequired={req.replyReqired}
+          replyRequired={req.replyRequired}
         />
       </div>
     </section>
+  )
+}
+
+function AtomicRequisitionHeading({
+  reqId,
+  clauseRef,
+  query,
+  level,
+  reqIsApplicable,
+}: {
+  reqId: string
+  clauseRef: string
+  query: string
+  level: number
+  reqIsApplicable: boolean
+}) {
+  return (
+    <div className="w-full">
+      <div className="flex flex-row">
+        <div className="mr-4 text-lg font-bold">{clauseRef}</div>
+        <div className="text-lg font-bold uppercase">{query}</div>
+      </div>
+    </div>
   )
 }
 
@@ -66,14 +105,14 @@ function AtomicRequisition({
   replyRequired,
 }: AtomicRequisitionProps) {
   return (
-    <div className="w-full p-4 focus-within:bg-slate-50 focus-within:border-x-2 focus-within:border-slate-300 border-box rounded-md">
-      <div className="flex flex-col gap-y-4 lg:gap-y-0 lg:gap-x-6 lg:flex-row lg:items-start">
-        <div className="lg:w-1/2 lg:flex lg:flex-row">
+    <div className="border-box w-full rounded-md focus-within:border-x-2 focus-within:border-slate-300 focus-within:bg-slate-50">
+      <div className="flex flex-col gap-y-4 px-4 pb-4 lg:flex-row lg:items-start lg:gap-x-6 lg:gap-y-0 lg:p-4">
+        <div className="lg:flex lg:w-1/2 lg:flex-row">
           <SectionSpacer level={level} />
           <SectionIndicator clauseRef={clauseRef} />
           <SectionQuery query={query} />
         </div>
-        <div className="lg:w-1/2 lg:flex lg:flex-row">
+        <div className="lg:flex lg:w-1/2 lg:flex-row">
           {replyRequired ? (
             <>
               <SectionReply reply={reply} reqId={reqId} />
@@ -88,7 +127,7 @@ function AtomicRequisition({
 
 function SectionSpacer({ level }: { level: number }) {
   const spacer_class = cn({
-    "lg:w-[0px]": level === 1,
+    "lg:w-[4px]": level === 1,
     "lg:w-[18px]": level === 2,
     "lg:w-[36px]": level === 3,
   })
@@ -97,19 +136,19 @@ function SectionSpacer({ level }: { level: number }) {
 
 function SectionIndicator({ clauseRef }: { clauseRef: string }) {
   return (
-    <div className="lg:w-[48px] shrink-0">
+    <div className="shrink-0 lg:mr-6 lg:min-w-[48px]">
       <div className="font-semibold">{clauseRef}</div>
     </div>
   )
 }
 
 function SectionQuery({ query }: { query: string }) {
-  return <div className="lg:w-9/12 px-4">{query}</div>
+  return <div className="lg:w-9/12">{query}</div>
 }
 
 function SectionReply({ reply, reqId }: { reply?: string; reqId: string }) {
   return (
-    <div className="lg:w-9/12 px-4">
+    <div className="lg:mr-6 lg:w-9/12">
       <ReplyForm reply={reply} reqId={reqId} />
     </div>
   )
@@ -117,24 +156,24 @@ function SectionReply({ reply, reqId }: { reply?: string; reqId: string }) {
 
 function SectionOptions() {
   return (
-    <div className="lg:w-[84px] shrink-0">
-      <div className="flex flex-row space-x-4 mt-3">
+    <div className="shrink-0 lg:w-[84px]">
+      <div className="mt-3 flex flex-row space-x-4">
         <CheckCircle
           size={20}
           color="black"
-          className="fill-teal-300 hover:cursor-pointer hover:fill-teal-100 transition"
+          className="fill-teal-300 transition hover:cursor-pointer hover:fill-teal-100"
           strokeWidth={1.15}
         />
         <FileText
           size={20}
           color="black"
           strokeWidth={1.25}
-          className="fill-teal-300 hover:cursor-pointer hover:fill-teal-100 transition"
+          className="fill-teal-300 transition hover:cursor-pointer hover:fill-teal-100"
         />
         <Flag
           size={20}
           strokeWidth={1.75}
-          className="fill-red-500 hover:fill-red-100 hover:cursor-pointer transition dark:text-gray-300 text-gray-900"
+          className="fill-red-500 text-gray-900 transition hover:cursor-pointer hover:fill-red-100 dark:text-gray-300"
         />
       </div>
     </div>
