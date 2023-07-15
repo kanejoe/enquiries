d2d7ab1b267d3b6733d854a9292989ac451e208d
kanejoe <kane_joseph@hotmail.com>
2023-07-15 05:55:25 +0100

    updating packages

--- app/atomic_requisition/Footer.tsx
+++ app/atomic_requisition/Footer.tsx
@@ -3,11 +3,17 @@ import { ChevronLeft, ChevronRight } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import { Separator } from "@/components/ui/separator"
 
+/**
+ *
+ * @returns
+ */
 export function Footer() {
+  const currentDate = new Date()
+  const currentYear = currentDate.getFullYear()
   return (
     <div className="mt-8 w-full">
       <Separator />
-      <div className="mt-2 flex justify-between">
+      <div className="mt-4 flex justify-between">
         <div className="flex flex-row gap-x-4">
           <Button variant="outline" size="sm">
             <ChevronLeft className="h-4 w-4" />
@@ -16,7 +22,7 @@ export function Footer() {
             <ChevronRight className="h-4 w-4" />
           </Button>
         </div>
-        <div className="text-sm">&copy; 2023</div>
+        <div className="text-sm">&copy; {currentYear}</div>
       </div>
     </div>
   )
