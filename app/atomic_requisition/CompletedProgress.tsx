96a1e0c3fc67ed0a956988af272d0677507261b3
kanejoe <kane_joseph@hotmail.com>
2023-08-02 20:30:31 +0100

    working on completed progress

--- app/atomic_requisition/CompletedProgress.tsx
+++ app/atomic_requisition/CompletedProgress.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/lib/utils"
 import { Progress } from "@/components/ui/progress"
 
 import { AtomicReq, useAtomicReqStore } from "./reqStore"
@@ -11,13 +12,20 @@ export function CompletedProgress() {
     (req: AtomicReq) => req.isComplete === true
   )
   const percent = calcPercent(completedReqs.length, totalReqs.length)
+  const headingReq = requisitions.find((req: AtomicReq) => req.level === 0)
 
   return (
-    <div className="mt-0.5 flex flex-col gap-y-1">
+    <div
+      className={
+        (cn("mt-0.5 flex flex-col gap-y-1 transition-opacity delay-200"),
+        headingReq?.isApplicable ? "opacity-100" : "opacity-0")
+      }
+    >
       <div className="text-sm">
         <span className="mr-1 oldstyle-nums">{percent}%</span>
         <span className="">complete</span>
       </div>
+
       <div className="">
         <Progress value={percent} className="" />
       </div>
