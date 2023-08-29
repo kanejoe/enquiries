294a634b69a6865ffc7308e829dadb18ff8ea600
kanejoe <kane_joseph@hotmail.com>
2023-08-29 22:10:02 +0100

    adding an add button

--- app/requisitions/create/page.tsx
+++ app/requisitions/create/page.tsx
@@ -1,11 +1,21 @@
-import { Suspense } from "react"
+import Link from "next/link"
+import { PlusCircledIcon } from "@radix-ui/react-icons"
 
-import LoadingSkeleton from "@/components/LoadingSkeleton"
+import { Button } from "@/components/ui/button"
 
 export default async function CreateRequisitionPage() {
   return (
-    <Suspense fallback={<p>Loading...</p>}>
-      <section className="">Add a button here</section>
-    </Suspense>
+    <section className="">
+      <Button
+        asChild
+        className="h-12 w-full px-4 py-2 text-primary-foreground shadow hover:bg-primary/90"
+        variant={"default"}
+      >
+        <Link href="/login">
+          <PlusCircledIcon className="mr-2 h-4 w-4" />
+          Add New
+        </Link>
+      </Button>
+    </section>
   )
 }
