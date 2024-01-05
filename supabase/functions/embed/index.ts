85ecfb8194e7195576e4e2e529a32ec3a348f85f
kanejoe <kane_joseph@hotmail.com>
2024-01-05 13:43:51 +0000

    getting working by turning off rls

--- supabase/functions/embed/index.ts
+++ supabase/functions/embed/index.ts
@@ -3,16 +3,27 @@
 // This enables autocomplete, go to definition, etc.
 
 import { createClient } from "@supabase/supabase-js"
+import { env, pipeline } from "@xenova/transformers"
 
-// import PDFLoader from "pdfLoader"
-
+import { corsHeaders } from "../_lib/cors.ts"
 import { Database } from "../_lib/database.ts"
 
 const supabaseUrl = Deno.env.get("SUPABASE_URL")
 console.log("🚀 ~ file: index.ts:12 ~ supabaseUrl:", supabaseUrl)
 const supabaseAnonKey = Deno.env.get("SUPABASE_ANON_KEY")
 
+// Configuration for Deno runtime
+env.useBrowserCache = false
+env.allowLocalModels = false
+
+const _pipe = await pipeline("feature-extraction", "Supabase/gte-small")
+
 Deno.serve(async (req) => {
+  // This is needed if you're planning to invoke your function from a browser.
+  if (req.method === "OPTIONS") {
+    return new Response("ok", { headers: corsHeaders })
+  }
+
   if (!supabaseUrl || !supabaseAnonKey) {
     return new Response(
       JSON.stringify({
@@ -37,11 +48,9 @@ Deno.serve(async (req) => {
     )
   }
 
-  const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
+  const supabaseClient = createClient<Database>(supabaseUrl, supabaseAnonKey, {
     global: {
-      headers: {
-        authorization,
-      },
+      headers: { Authorization: req.headers.get("Authorization")! },
     },
     auth: {
       persistSession: false,
@@ -50,13 +59,14 @@ Deno.serve(async (req) => {
 
   const { document_id } = await req.json()
 
-  const { data: document } = await supabase
-    .from("documents_with_storage_path_and_created_by_email")
+  const { data: document } = await supabaseClient
+    .from("documents")
     .select()
     .eq("id", document_id)
     .single()
+  console.log("🚀 ~ file: index.ts:57 ~ Deno.serve ~ document:", document)
 
-  if (!document?.storage_object_path) {
+  if (!document) {
     return new Response(
       JSON.stringify({ error: "Failed to find uploaded document" }),
       {
@@ -66,42 +76,28 @@ Deno.serve(async (req) => {
     )
   }
 
-  const { data: file } = await supabase.storage
-    .from("files")
-    .download(document.storage_object_path)
-
-  if (!file) {
-    return new Response(
-      JSON.stringify({ error: "Failed to download storage object" }),
-      {
-        status: 500,
-        headers: { "Content-Type": "application/json" },
-      }
-    )
-  }
-
-  // const _loader = new PDFLoader(file, {
-  //   splitPages: false,
+  // Generate the embedding from the user input
+  // const output = await pipe(input, {
+  //   pooling: "mean",
+  //   normalize: true,
   // })
-  // const docs = await loader.load()
-  // console.log("🚀 ~ file: index.ts:87 ~ Deno.serve ~ docs:", docs)
 
-  // const _fileContents = await file.text()
+  // // Extract the embedding output
+  // const embedding = Array.from(output.data)
 
-  return new Response(null, {
-    status: 204,
+  return new Response(JSON.stringify(document), {
     headers: { "Content-Type": "application/json" },
   })
 })
 
 /* To invoke locally:
 
-  1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
+  1. Run `npx supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
   2. Make an HTTP request:
 
-  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/process' \
-    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
-    --header 'Content-Type: application/json' \
-    --data '{"document_id":"28"}'
+  curl --request POST 'http://localhost:54321/functions/v1/embed' \
+  --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
+  --header 'Content-Type: application/json' \
+  --data '{ "document_id": 28 }'
 
 */
---
+++ supabase/functions/tests/function-embed-test.ts
@@ -0,0 +1,50 @@
+// Import required libraries and modules
+
+// import {
+//   createClient,
+//   SupabaseClient,
+// } from "https://esm.sh/@supabase/supabase-js@2.23.0"
+
+import { createClient } from "@supabase/supabase-js"
+import { assert } from "https://deno.land/std@0.192.0/testing/asserts.ts"
+
+import { Database } from "../_lib/database.ts"
+
+// Set up the configuration for the Supabase client
+const supabaseUrl = Deno.env.get("SUPABASE_URL") ?? "http://127.0.0.1:54321"
+const supabaseKey =
+  Deno.env.get("SUPABASE_ANON_KEY") ??
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0"
+const options = {
+  auth: {
+    autoRefreshToken: false,
+    persistSession: false,
+    detectSessionInUrl: false,
+  },
+}
+
+// Test the creation and functionality of the Supabase client
+const testClientCreation = async () => {
+  const client = createClient<Database>(supabaseUrl, supabaseKey, options)
+
+  // Verify if the Supabase URL and key are provided
+  if (!supabaseUrl) throw new Error("supabaseUrl is required.")
+  if (!supabaseKey) throw new Error("supabaseKey is required.")
+
+  // Test a simple query to the database
+  const { data: table_data, error: table_error } = await client
+    .from("documents")
+    .select("*")
+  // .limit(1)
+  console.log(
+    "🚀 ~ file: function-embed-test.ts:39 ~ testClientCreation ~ table_data:",
+    table_data
+  )
+  if (table_error) {
+    throw new Error("Invalid Supabase client: " + table_error.message)
+  }
+  assert(table_data, "Data should be returned from the query.")
+}
+
+// Register and run the tests
+Deno.test("Client Creation Test", testClientCreation)
