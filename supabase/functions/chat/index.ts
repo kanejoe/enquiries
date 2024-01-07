6b4a42692afbda8e72e4be8df675b47b728e743f
kanejoe <kane_joseph@hotmail.com>
2024-01-07 17:27:27 +0000

    adding match migration

--- supabase/functions/chat/index.ts
+++ supabase/functions/chat/index.ts
@@ -1,32 +1,129 @@
-// import OpenAI from "https://deno.land/x/openai@v4.24.0/mod.ts"
-import { StringOutputParser } from "https://esm.sh/@langchain/core@0.1.8/output_parsers"
-import { ChatPromptTemplate } from "https://esm.sh/@langchain/core@0.1.8/prompts"
-import { ChatOpenAI } from "npm:@langchain/openai"
+import { createClient } from "@supabase/supabase-js"
+import { OpenAIStream, StreamingTextResponse } from "ai"
+import { codeBlock } from "common-tags"
+import OpenAI from "openai"
 
-// import { TextLoader } from "npm:langchain/document_loaders/fs/text"
+import { Database } from "../_lib/database.ts"
 
-// import { ChatOpenAI } from "https://esm.sh/@langchain/openai@0.0.9"
+const openai = new OpenAI({
+  apiKey: Deno.env.get("OPENAI_API_KEY"),
+})
+
+// These are automatically injected
+const supabaseUrl = Deno.env.get("SUPABASE_URL")
+const supabaseAnonKey = Deno.env.get("SUPABASE_ANON_KEY")
+
+export const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers":
+    "authorization, x-client-info, apikey, content-type",
+}
 
 Deno.serve(async (req) => {
-  const { _input } = await req.json()
+  // Handle CORS
+  if (req.method === "OPTIONS") {
+    return new Response("ok", { headers: corsHeaders })
+  }
+  if (!supabaseUrl || !supabaseAnonKey) {
+    return new Response(
+      JSON.stringify({
+        error: "Missing environment variables.",
+      }),
+      {
+        status: 500,
+        headers: { "Content-Type": "application/json" },
+      }
+    )
+  }
 
-  const prompt = ChatPromptTemplate.fromMessages([
-    ["human", "Tell me a really funny joke about {topic}"],
-  ])
-  const model = new ChatOpenAI({})
-  const outputParser = new StringOutputParser()
+  const authorization = req.headers.get("Authorization")
 
-  const chain = prompt.pipe(model).pipe(outputParser)
+  if (!authorization) {
+    return new Response(
+      JSON.stringify({ error: `No authorization header passed` }),
+      {
+        status: 500,
+        headers: { "Content-Type": "application/json" },
+      }
+    )
+  }
 
-  const response = await chain.invoke({
-    topic: "twins",
+  const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
+    global: {
+      headers: {
+        authorization,
+      },
+    },
+    auth: {
+      persistSession: false,
+    },
   })
-  console.log(response)
-  /**
-Why did the ice cream go to the gym?
-Because it wanted to get a little "cone"ditioning!
- */
-  return new Response(response, {
-    headers: { "Content-Type": "text/plain" },
+
+  const { messages, embedding } = await req.json()
+
+  const { data: documents, error: matchError } = await supabase
+    .rpc("match_document_sections", {
+      embedding,
+      match_threshold: 0.8,
+    })
+    .select("content")
+    .limit(5)
+
+  if (matchError) {
+    console.error(matchError)
+
+    return new Response(
+      JSON.stringify({
+        error: "There was an error reading your documents, please try again.",
+      }),
+      {
+        status: 500,
+        headers: { "Content-Type": "application/json" },
+      }
+    )
+  }
+
+  const injectedDocs =
+    documents && documents.length > 0
+      ? documents.map(({ content }) => content).join("\n\n")
+      : "No documents found"
+
+  console.log(injectedDocs)
+
+  const completionMessages: OpenAI.Chat.Completions.ChatCompletionMessageParam[] =
+    [
+      {
+        role: "user",
+        content: codeBlock`
+        You're an AI assistant who answers questions about documents.
+
+        You're a chat bot, so keep your replies succinct.
+
+        You're only allowed to use the documents below to answer the question.
+
+        If the question isn't related to these documents, say:
+        "Sorry, I couldn't find any information on that."
+
+        If the information isn't available in the below documents, say:
+        "Sorry, I couldn't find any information on that."
+
+        Do not go off topic.
+
+        Documents:
+        ${injectedDocs}
+      `,
+      },
+      ...messages,
+    ]
+
+  const completionStream = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo-0613",
+    messages: completionMessages,
+    max_tokens: 1024,
+    temperature: 0,
+    stream: true,
   })
+
+  const stream = OpenAIStream(completionStream)
+  return new StreamingTextResponse(stream, { headers: corsHeaders })
 })
