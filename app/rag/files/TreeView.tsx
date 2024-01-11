d99b51e825c654c42882632f7062f0f8512685f3
kanejoe <kane_joseph@hotmail.com>
2024-01-11 11:50:13 +0000

    adding treeview initially

--- app/rag/files/TreeView.tsx
+++ app/rag/files/TreeView.tsx
@@ -3,19 +3,15 @@
  * The Root component holds an initial ul wrapping the root nodes.
  */
 
-import { ReactNode, useState } from "react"
+import { ReactNode, useReducer, useState } from "react"
 
 import { cn } from "@/lib/utils"
 
-export const TreeView = ({ treeData }: { treeData: TreeNodeType[] }) => {
-  const [selected, select] = useState<string | null>(null)
+import { treeviewReducer } from "./TreeViewProvider"
 
+export const TreeView = ({ treeData }: { treeData: TreeNodeType[] }) => {
   return (
-    <Root
-      className="m-8 h-full w-72 border-[1.5px] border-slate-100"
-      value={selected}
-      onChange={select}
-    >
+    <Root className="m-8 h-full w-72 border-[1.5px] border-slate-100">
       {treeData.map((node) => (
         <Node node={node} key={node.id} />
       ))}
@@ -26,11 +22,13 @@ export const TreeView = ({ treeData }: { treeData: TreeNodeType[] }) => {
 type RootProps = {
   children: ReactNode | ReactNode[]
   className?: string
-  value: string | null
-  onChange: (value: string | null) => void
 }
 
 export function Root({ children, className }: RootProps) {
+  const [open, dispatch] = useReducer(
+    treeviewReducer,
+    new Map<string, boolean>()
+  )
   return (
     <ul className={cn("flex flex-col overflow-auto", className)}>{children}</ul>
   )
---
+++ app/rag/files/TreeViewProvider.tsx
@@ -0,0 +1,48 @@
+import { createContext, Dispatch } from "react"
+
+// TreeViewState is a Map of ids (string) to open state (boolean) of a particular Node.
+export type TreeViewState = Map<string, boolean>
+
+export enum TreeViewActionTypes {
+  OPEN = "OPEN",
+  CLOSE = "CLOSE",
+}
+
+export type TreeViewActions =
+  | {
+      type: TreeViewActionTypes.OPEN
+      id: string
+    }
+  | {
+      type: TreeViewActionTypes.CLOSE
+      id: string
+    }
+
+// treeviewReducer is a reducer that takes in a TreeViewState and a TreeViewAction and returns a new TreeViewState.
+export function treeviewReducer(
+  state: TreeViewState,
+  action: TreeViewActions
+): TreeViewState {
+  switch (action.type) {
+    case TreeViewActionTypes.OPEN:
+      return new Map(state).set(action.id, true)
+
+    case TreeViewActionTypes.CLOSE:
+      return new Map(state).set(action.id, false)
+
+    default:
+      throw new Error("Tree Reducer received an unknown action")
+  }
+}
+
+// TreeViewContextType is the type of the context that is used to provide the TreeView state and dispatch to the TreeView component.
+export type TreeViewContextType = {
+  open: TreeViewState
+  dispatch: Dispatch<TreeViewActions>
+}
+
+// TreeViewContext is the context that is used to provide the TreeView state and dispatch to the TreeView component.
+export const TreeViewContext = createContext<TreeViewContextType>({
+  open: new Map<string, boolean>(),
+  dispatch: () => {},
+})
