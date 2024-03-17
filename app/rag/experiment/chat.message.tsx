92e794bdcfae892aff8100cb8ef22c2ca5eb46eb
kanejoe <kane_joseph@hotmail.com>
2024-03-17 16:11:50 +0000

    adding cva sort of

--- app/rag/experiment/chat.message.tsx
+++ app/rag/experiment/chat.message.tsx
@@ -2,6 +2,7 @@ import { FC } from "react"
 import type { Message } from "ai/react"
 
 import { cn } from "@/lib/utils"
+import { ChatHeaderIcon } from "@/components/chat/chat-bubble-icons"
 import { ChatMessageBubble } from "@/components/chat/chat-message-bubble"
 
 interface ChatMessageBubbleSpecProps {}
@@ -35,18 +36,32 @@ interface ChatMessageBubbleSpecProps {
 }
 
 const ChatMessageBubbleSpec: FC<ChatMessageBubbleSpecProps> = ({
-  height = "600px",
+  height = "540px",
 }: ChatMessageBubbleSpecProps) => {
   const divStyle = { maxHeight: height }
   return (
-    <div className="mx-auto max-w-3xl rounded-2xl border border-zinc-200 bg-zinc-50/25 bg-gradient-to-r from-[#D7EDEA] to-[#F4FBDF] py-8 pl-8 pr-2 shadow-lg">
-      <div
-        style={divStyle} // Apply inline style here
-        className={`flex flex-col gap-y-8 overflow-y-auto scrollbar-thin scrollbar-track-zinc-50 scrollbar-thumb-zinc-100`}
-      >
-        <ChatMessageBubble message={messageUser} />
-        <ChatMessageBubble message={messageAi} sources={sources} />
-        <ChatMessageBubble message={messageUser1} />
+    <div className="mx-auto flex max-w-3xl flex-col gap-y-6">
+      <div className="rounded-2xl border border-zinc-200 bg-zinc-50/25 bg-gradient-to-r from-[#D7EDEA] to-[#F4FBDF] px-4 py-4 shadow-sm transition-shadow hover:shadow-md">
+        <div className="absolute inset-0 rounded-2xl ring-2 ring-inset ring-zinc-900/20 group-hover:ring-zinc-900/10 dark:ring-white/10 dark:group-hover:ring-white/20" />
+        <h2 className="flex flex-row">
+          <div className="w-16 shrink-0">
+            <ChatHeaderIcon />
+          </div>
+          <div className="mt-1 overflow-hidden overflow-ellipsis whitespace-nowrap pr-8 font-albertsans text-xl font-semibold text-zinc-600">
+            There may be just 40 Russian athletes at Paris 2024, claims IOC
+            vice-president
+          </div>
+        </h2>
+      </div>
+      <div className="rounded-2xl border border-zinc-200 bg-zinc-50/25 bg-gradient-to-r from-[#D7EDEA] to-[#F4FBDF] py-8 pl-8 pr-2 shadow-lg">
+        <div
+          style={divStyle} // Apply inline style here
+          className={`flex flex-col gap-y-8 overflow-y-auto scrollbar-thin scrollbar-track-zinc-50 scrollbar-thumb-zinc-100`}
+        >
+          <ChatMessageBubble message={messageUser} />
+          <ChatMessageBubble message={messageAi} sources={sources} />
+          <ChatMessageBubble message={messageUser1} />
+        </div>
       </div>
     </div>
   )
--- components/chat/chat-bubble-icons.tsx
+++ components/chat/chat-bubble-icons.tsx
@@ -1,51 +1,104 @@
-import { ElementType } from "react"
+"use client"
 
+import { ElementType, FC, HTMLAttributes } from "react"
+import { cva, type VariantProps } from "class-variance-authority"
+
+import { cn } from "@/lib/utils"
+import { BoltIcon } from "@/components/icons/BoltIcon"
 import { ChatBubbleIcon } from "@/components/icons/ChatBubbleIcon"
 import { CheckIcon } from "@/components/icons/CheckIcon"
 import { ClipboardIcon } from "@/components/icons/ClipboardIcon"
 import { MagnifyingGlassIcon } from "@/components/icons/MagnifyingGlassIcon"
 import { UserIcon } from "@/components/icons/UserIcon"
 
-type IconContainerProps = {
+const iconContainerVariants = cva("", {
+  variants: {
+    size: {
+      default: "w-6 h-6",
+      md: "w-8 h-8",
+      lg: "w-10 h-10",
+    },
+    icon: {
+      default: "w-4 h-4",
+      md: "w-5 h-5",
+      lg: "w-6 h-6",
+    },
+  },
+  // defaultVariants: {
+  //   size: "lg",
+  //   icon: "default",
+  // },
+})
+
+interface IconContainerProps
+  extends HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof iconContainerVariants> {
   IconComponent: ElementType // Defines a prop that can take any React component
-  size?: string
-  iconSize?: string
 }
 
-function IconContainer({
+const IconContainer: FC<IconContainerProps> = ({
   IconComponent,
-  size = "6",
-  iconSize = "4",
+  size,
+  icon,
   ...props
-}: IconContainerProps) {
+}) => {
   return (
     <div
-      className={`dark:bg-white/7.5 flex size-${size} items-center justify-center rounded-full bg-zinc-900/5 ring-1 ring-zinc-900/25 backdrop-blur-[2px] transition duration-300 group-hover:bg-white/50 group-hover:ring-zinc-900/25 dark:ring-white/15 dark:group-hover:bg-emerald-300/10 dark:group-hover:ring-emerald-400`}
+      className={cn(
+        "dark:bg-white/7.5 flex items-center justify-center rounded-full bg-zinc-900/5 ring-1 ring-zinc-900/25 backdrop-blur-[2px] transition duration-300 group-hover:bg-white/50 group-hover:ring-zinc-900/25 dark:ring-white/15 dark:group-hover:bg-emerald-300/10 dark:group-hover:ring-emerald-400",
+        iconContainerVariants({ size })
+      )}
       {...props}
     >
       <IconComponent
-        className={`size-${iconSize} fill-zinc-700/10 stroke-zinc-700 transition-colors duration-300 group-hover:stroke-zinc-900 dark:fill-white/10 dark:stroke-zinc-400 dark:group-hover:fill-emerald-300/10 dark:group-hover:stroke-emerald-400`}
+        className={cn(
+          "dark:group-hover:stroke-emerald-400` fill-zinc-700/10 stroke-zinc-700 transition-colors duration-300 group-hover:stroke-zinc-900 dark:fill-white/10 dark:stroke-zinc-400 dark:group-hover:fill-emerald-300/10",
+          iconContainerVariants({ icon })
+        )}
       />
     </div>
   )
 }
 
+export function ChatHeaderIcon() {
+  return <IconContainer IconComponent={BoltIcon} size="lg" icon="lg" />
+}
 export function ChatUserIcon() {
-  return <IconContainer IconComponent={UserIcon} size="10" iconSize="6" />
+  return <IconContainer IconComponent={UserIcon} size="lg" icon="lg" />
 }
 
 export function ChatAiIcon() {
-  return <IconContainer IconComponent={ChatBubbleIcon} size="10" iconSize="6" />
+  return (
+    <IconContainer IconComponent={ChatBubbleIcon} size={"lg"} icon={"lg"} />
+  )
 }
 
 export function SourceIcon() {
-  return <IconContainer IconComponent={MagnifyingGlassIcon} />
+  return (
+    <IconContainer
+      IconComponent={MagnifyingGlassIcon}
+      size={"default"}
+      icon={"default"}
+    />
+  )
 }
 
 export function CopyIcon() {
-  return <IconContainer IconComponent={ClipboardIcon} size="6" iconSize="4" />
+  return (
+    <IconContainer
+      IconComponent={ClipboardIcon}
+      size={"default"}
+      icon={"default"}
+    />
+  )
 }
 
 export function PasteIcon() {
-  return <IconContainer IconComponent={CheckIcon} size="8" iconSize="6" />
+  return (
+    <IconContainer
+      IconComponent={CheckIcon}
+      size={"default"}
+      icon={"default"}
+    />
+  )
 }
